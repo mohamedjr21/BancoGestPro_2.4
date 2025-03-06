951a354f4831071cc828bcf89197a5bc8c5a8cb4
mohamedjr21 <mohamedjr21@example.com>
2025-03-06 10:11:49 +0100

    Inicio del proceso de mejorar la practica, aplicar bien los bidings y mejorar el codigo, hacer más commits.  Y que quede con una interfaz chula y moderna. Comenzando............

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,7 @@ module com.example.bancomfh {
 
   exports com.example.bancomfh;
   exports Controladores;
+  //exports Tests;
   opens Controladores to javafx.fxml, org.junit.platform.commons;
   opens modelo to javafx.base;
 
